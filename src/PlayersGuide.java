4a5966ae34d137521e3c3656eac8423baf5c1819
J.A. van der Mullen <info@jera-hardware.nl>
2022-10-13 13:18:33 +0200

    Day 2 2/3

--- src/PlayersGuide.java
+++ src/PlayersGuide.java
@@ -1,6 +1,6 @@
 public class PlayersGuide {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Day 2: 2/3 - Challenge What Comes Next");
     }
 }
